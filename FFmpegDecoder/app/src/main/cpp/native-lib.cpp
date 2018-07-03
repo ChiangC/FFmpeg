49b06c3e088ee3582f55e93c18281008c61b5ca9
Drew.Chiang <drew_jiang@mtelnet.com>
2018-07-03 17:21:22 +0800

    ffmpeg decoder.

--- FFmpegDecoder/app/src/main/cpp/native-lib.cpp
+++ FFmpegDecoder/app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,103 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
-extern "C" JNIEXPORT jstring
+extern "C"{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+}
 
-JNICALL
-Java_com_fmtech_ffmpegdecoder_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+#define LOGI(FORMAT, ...) __android_log_print(ANDROID_LOG_INFO, "FFmpegDecoder", FORMAT, ##__VA_ARGS__);
+#define LOGE(FORMAT, ...) __android_log_print(ANDROID_LOG_ERROR, "FFmpegDecoder", FORMAT, ##__VA_ARGS__);
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_fmtech_ffmpegdecoder_MainActivity_mp4ToYuv(JNIEnv *env, jobject instance,
+                                                    jstring inputPath_, jstring outPutPath_) {
+    const char *inputPath = env->GetStringUTFChars(inputPath_, 0);
+    const char *outPutPath = env->GetStringUTFChars(outPutPath_, 0);
+
+    av_register_all();
+
+    AVFormatContext *pContext = avformat_alloc_context();
+
+    //Open an input stream and read the header. The codecs are not opened.
+    if(avformat_open_input(&pContext, inputPath, NULL, NULL) < 0){
+        LOGE("Open input failed.");
+        return;
+    }
+
+
+    //Read packets of a media file to get stream information.
+    if(avformat_find_stream_info(pContext, NULL) < 0){
+        LOGE("Get stream information failed.");
+        return;
+    }
+
+    int video_stream_idx = -1;
+    //Find video stream
+    for(int i=0; i < pContext->nb_streams; i++){
+        if(pContext->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO){
+            video_stream_idx = i;
+        }
+    }
+
+    AVCodecContext *pCodecCtx = pContext->streams[video_stream_idx]->codec;
+
+    AVCodec *pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+    if(avcodec_open2(pCodecCtx, pCodec, NULL) < 0){
+        LOGE("avcodec open failed.");
+        return;
+    }
+
+    AVPacket *packet = (AVPacket*)av_malloc(sizeof(AVPacket));
+    av_init_packet(packet);
+
+    AVFrame *frame = av_frame_alloc();
+    AVFrame *yuvFrame = av_frame_alloc();
+
+    uint8_t *out_buffer = (uint8_t *)av_malloc(avpicture_get_size(AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height));
+
+    /**
+    * Setup the picture fields based on the specified image parameters
+    * and the provided image data buffer.
+    */
+    int re = avpicture_fill((AVPicture*)yuvFrame, out_buffer, AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height);
+
+    SwsContext *swsContext = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
+                                            pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P,
+                                            SWS_BILINEAR, NULL, NULL, NULL);
+
+    int frameCount = 0;
+    FILE *fp_yuv = fopen(outPutPath, "wb");
+
+    int got_frame;
+    while(av_read_frame(pContext, packet) >= 0){
+        avcodec_decode_video2(pCodecCtx, frame, &got_frame, packet);
+
+        if(got_frame > 0){
+            /**
+            * Scale the image slice in srcSlice and put the resulting scaled
+            * slice in the image in dst. A slice is a sequence of consecutive
+            * rows in an image.
+            */
+            sws_scale(swsContext, (const uint8_t *const *) frame->data, frame->linesize, 0, frame->height, yuvFrame->data, yuvFrame->linesize);
+            int y_size = pCodecCtx->width * pCodecCtx->height;
+            fwrite(yuvFrame->data[0], 1, y_size, fp_yuv);
+            fwrite(yuvFrame->data[1], 1, y_size/4, fp_yuv);
+            fwrite(yuvFrame->data[2], 1, y_size/4, fp_yuv);
+        }
+
+        av_free_packet(packet);
+    }
+
+    fclose(fp_yuv);
+    av_frame_free(&frame);
+    av_frame_free(&yuvFrame);
+    avcodec_close(pCodecCtx);
+    avformat_free_context(pContext);
+
+    env->ReleaseStringUTFChars(inputPath_, inputPath);
+    env->ReleaseStringUTFChars(outPutPath_, outPutPath);
 }
--- FFmpegDecoder/app/src/main/java/com/fmtech/ffmpegdecoder/MainActivity.java
+++ FFmpegDecoder/app/src/main/java/com/fmtech/ffmpegdecoder/MainActivity.java
@@ -1,13 +1,25 @@
 package com.fmtech.ffmpegdecoder;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
     static {
+        System.loadLibrary("avcodec-56");
+        System.loadLibrary("avdevice-56");
+        System.loadLibrary("avfilter-5");
+        System.loadLibrary("avformat-56");
+        System.loadLibrary("avutil-54");
+        System.loadLibrary("postproc-53");
+        System.loadLibrary("swresample-1");
+        System.loadLibrary("swscale-3");
         System.loadLibrary("native-lib");
     }
 
@@ -16,14 +28,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        checkPermission();
+
+        TextView decodeBtn = findViewById(R.id.btn_decode);
+        decodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/self_driving.mp4";
+                String outputPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/self_driving.yuv";
+                mp4ToYuv(inputPath, outputPath);
+            }
+        });
+
+        String hello = "hello";
+        String he ="he" + new String("llo");
+        System.out.println("-------"+(hello == he));
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void checkPermission(){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        }
+    }
+
+
+    public native void mp4ToYuv(String inputPath, String outPutPath);
+
+
 }
