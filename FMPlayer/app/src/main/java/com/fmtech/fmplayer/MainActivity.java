8cc7d62e43812dc6660e6f5a7831ae9c70104944
AI.Tsingh.ChiangC <jiangchuna@126.com>
2018-07-03 22:23:13 +0800

    fmplayer.

--- FMPlayer/app/src/main/java/com/fmtech/fmplayer/MainActivity.java
+++ FMPlayer/app/src/main/java/com/fmtech/fmplayer/MainActivity.java
@@ -1,29 +1,46 @@
 package com.fmtech.fmplayer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+
+import com.fmtech.fmplayer.view.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mVideoView = (VideoView) findViewById(R.id.video_view);
+
+        checkPermission();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void checkPermission(){
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+        }else{
+            playVideo();
+        }
+    }
+
+    private void playVideo(){
+        mVideoView.playVideo(Environment.getExternalStorageDirectory().getAbsolutePath() +"/self_driving.mp4");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            playVideo();
+        }
+    }
 }
---
+++ FMPlayer/app/src/main/java/com/fmtech/fmplayer/view/VideoView.java
@@ -0,0 +1,66 @@
+package com.fmtech.fmplayer.view;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import org.w3c.dom.Text;
+
+/**
+ * ==================================================================
+ * Copyright (C) 2018 FMTech All Rights Reserved.
+ *
+ * @author Drew.Chiang
+ * @version v1.0.0
+ * @email chiangchuna@gmail.com
+ * @create_date 2018/7/3 21:49
+ * <p>
+ * ==================================================================
+ */
+
+public class VideoView extends SurfaceView {
+    static{
+        System.loadLibrary("avcodec-56");
+        System.loadLibrary("avdevice-56");
+        System.loadLibrary("avfilter-5");
+        System.loadLibrary("avformat-56");
+        System.loadLibrary("avutil-54");
+        System.loadLibrary("postproc-53");
+        System.loadLibrary("swresample-1");
+        System.loadLibrary("swscale-3");
+        System.loadLibrary("native-lib");
+    }
+
+    public VideoView(Context context) {
+        super(context);
+    }
+
+    public VideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init(){
+        SurfaceHolder holder = getHolder();
+        holder.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    public void playVideo(final String url){
+        if(!TextUtils.isEmpty(url)){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+//        绘制功能 不需要交给SurfaveView        VideoView.this.getHolder().getSurface()
+                    render(url, VideoView.this.getHolder().getSurface());
+                }
+            }).start();
+        }
+    }
+
+    public native void render(String videoUrl, Surface surface);
+
+}
