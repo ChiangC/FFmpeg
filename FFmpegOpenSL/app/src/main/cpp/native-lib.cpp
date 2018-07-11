e5da5341903d1dcd2e270cef190d9233ca4c3c5b
AI.Tsingh.ChiangC <jiangchuna@126.com>
2018-07-11 21:49:54 +0800

    open sl play music ok!

--- FFmpegOpenSL/app/src/main/cpp/native-lib.cpp
+++ FFmpegOpenSL/app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <android/log.h>
 extern "C" {
-//编码
 #include "libavcodec/avcodec.h"
 #include "libavformat/avformat.h"
 #include "libswresample/swresample.h"
@@ -18,17 +17,17 @@ extern "C" {
 #define LOGE(FORMAT,...) __android_log_print(ANDROID_LOG_ERROR,"FMMusicPlayer",FORMAT,##__VA_ARGS__);
 
 SLObjectItf engineObject = NULL;
-SLEngineItf slEngineItf = NULL;
+SLEngineItf engineEngine = NULL;
 //混音器
 SLObjectItf outputMixObject = NULL;
 SLEnvironmentalReverbItf outputMixEnvironmentalReverbItf = NULL;
 //SLEnvironmentalReverbSettings reverbSettings = SL_I3DL2_ENVIRONMENT_PRESET_DEFAULT;
 SLEnvironmentalReverbSettings reverbSettings = SL_I3DL2_ENVIRONMENT_PRESET_STONECORRIDOR;
 
-SLObjectItf slPlayerObject;
+SLObjectItf bqPlayerObject;
 SLPlayItf  bqPlayerPlay;
 //队列缓冲区
-SLAndroidSimpleBufferQueueItf  bqPlayerQueue;
+SLAndroidSimpleBufferQueueItf  bqPlayerBufferQueue;
 //音量对象
 SLVolumeItf bqPlayerVolume;
 
@@ -38,7 +37,7 @@ void bqPlayerCallback(SLAndroidSimpleBufferQueueItf bufferQueueItf, void *contex
     bufferSize = 0;
     getPcm(&buffer, &bufferSize);
     if(NULL != buffer && 0 != bufferSize){
-        SLresult sLresult = (*bqPlayerQueue)->Enqueue(bqPlayerQueue, buffer, bufferSize);
+        SLresult sLresult = (*bqPlayerBufferQueue)->Enqueue(bqPlayerBufferQueue, buffer, bufferSize);
     }
 }
 
@@ -55,14 +54,14 @@ Java_com_fmtech_ffmpegopensl_MusicPlayer_play(JNIEnv *env, jobject instance, jst
     slCreateEngine(&engineObject, 0, NULL, 0, NULL, NULL);
     (*engineObject)->Realize(engineObject, SL_BOOLEAN_FALSE);
     //获取引擎接口
-    (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, &slEngineItf);
+    (*engineObject)->GetInterface(engineObject, SL_IID_ENGINE, &engineEngine);
 
 
     //创建混音器
     const SLInterfaceID mids[1] = {SL_IID_ENVIRONMENTALREVERB};
     const SLboolean mreq[1] = {SL_BOOLEAN_FALSE};
-//    (*slEngineItf)->CreateOutputMix(slEngineItf, &outputMixObject, 0, 0, 0);
-    (*slEngineItf)->CreateOutputMix(slEngineItf, &outputMixObject, 1, mids, mreq);
+//    (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, 0, 0, 0);
+    (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, 1, mids, mreq);
     (*outputMixObject)->Realize(outputMixObject, SL_BOOLEAN_FALSE);
 
     //设置环境混响
@@ -81,60 +80,58 @@ Java_com_fmtech_ffmpegopensl_MusicPlayer_play(JNIEnv *env, jobject instance, jst
     }
 
 
-    SLDataLocator_AndroidBufferQueue androidBufferQueue = {SL_DATALOCATOR_ANDROIDBUFFERQUEUE, 2};
+    SLDataLocator_AndroidBufferQueue androidBufferQueue = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
 
     SLDataFormat_PCM slDataFormatPcm = {
             SL_DATAFORMAT_PCM,
             channels,
-//            SL_SAMPLINGRATE_44_1,
-            SL_SAMPLINGRATE_8,
+            SL_SAMPLINGRATE_44_1,//Very important
+//            SL_SAMPLINGRATE_8,
             SL_PCMSAMPLEFORMAT_FIXED_16,
             SL_PCMSAMPLEFORMAT_FIXED_16,
             SL_SPEAKER_FRONT_LEFT|SL_SPEAKER_FRONT_RIGHT,
             SL_BYTEORDER_LITTLEENDIAN
     };
 
-    SLDataSource slDataSource = {&androidBufferQueue, &slDataFormatPcm};
+    SLDataSource audioSrc = {&androidBufferQueue, &slDataFormatPcm};
 
     //设置混音器
     SLDataLocator_OutputMix outputMix = {SL_DATALOCATOR_OUTPUTMIX, outputMixObject};
 
-    SLDataSink slDataSink = {&outputMix, NULL};
-    const SLInterfaceID interfaceIDs[3] = {SL_IID_BUFFERQUEUE, SL_IID_EFFECTSEND, SL_IID_VOLUME};
+    SLDataSink audioSnk = {&outputMix, NULL};
+    const SLInterfaceID interfaceIDs[3] = {SL_IID_BUFFERQUEUE, SL_IID_VOLUME, SL_IID_EFFECTSEND};
     const SLboolean req[3] = {SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE};
 
     /*关联混音器*/
     //创建一个播放器
-    sLresult = (*slEngineItf)->CreateAudioPlayer(slEngineItf, &slPlayerObject, &slDataSource, &slDataSink, 2, interfaceIDs, req);
+    sLresult = (*engineEngine)->CreateAudioPlayer(engineEngine, &bqPlayerObject, &audioSrc, &audioSnk, 3, interfaceIDs, req);
 
 
-    if(SL_RESULT_SUCCESS == sLresult && NULL != slPlayerObject){
-        LOGI("创建一个播放器成功");
+    if(SL_RESULT_SUCCESS == sLresult && NULL != bqPlayerObject){
+        LOGI("CreateAudioPlayer success");
     }else{
         LOGE("CreateAudioPlayer failed");
         return;
     }
 
-    sLresult = (*slPlayerObject)->Realize(slPlayerObject, SL_BOOLEAN_FALSE);
-    LOGI("播放器Realize成功");
+    // realize the player
+    sLresult = (*bqPlayerObject)->Realize(bqPlayerObject, SL_BOOLEAN_FALSE);
 
-    //创建播放器接口
-    sLresult = (*slPlayerObject)->GetInterface(slPlayerObject, SL_IID_PLAY, &bqPlayerPlay);
-    LOGI("创建播放器接口成功");
+    // get the play interface
+    sLresult = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_PLAY, &bqPlayerPlay);
 
-    //注册缓冲区
-    sLresult = (*slPlayerObject)->GetInterface(slPlayerObject, SL_IID_BUFFERQUEUE, &bqPlayerPlay);
-    LOGI("注册缓冲区成功");
+    // get the buffer queue interface
+    sLresult = (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_BUFFERQUEUE, &bqPlayerBufferQueue);
 
-    //设置回调接口
-    sLresult = (*bqPlayerQueue)->RegisterCallback(bqPlayerQueue, bqPlayerCallback, NULL);
-    LOGI("设置回调接口成功");
+    // register callback on the buffer queue
+    sLresult = (*bqPlayerBufferQueue)->RegisterCallback(bqPlayerBufferQueue, bqPlayerCallback, NULL);
 
-    (*slPlayerObject)->GetInterface(slPlayerObject, SL_IID_VOLUME, &bqPlayerVolume);
+    // get the volume interface
+    (*bqPlayerObject)->GetInterface(bqPlayerObject, SL_IID_VOLUME, &bqPlayerVolume);
     (*bqPlayerPlay)->SetPlayState(bqPlayerPlay, SL_PLAYSTATE_PLAYING);
+
     //播放第一帧
-    bqPlayerCallback(bqPlayerQueue, NULL);
-    LOGI("播放第一帧成功");
+    bqPlayerCallback(bqPlayerBufferQueue, NULL);
 
 
     env->ReleaseStringUTFChars(path_, path);
@@ -145,11 +142,11 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_fmtech_ffmpegopensl_MusicPlayer_stop(JNIEnv *env, jobject instance) {
 
-    if(NULL != slPlayerObject){
-        (*slPlayerObject)->Destroy(slPlayerObject);
-        slPlayerObject = NULL;
+    if(NULL != bqPlayerObject){
+        (*bqPlayerObject)->Destroy(bqPlayerObject);
+        bqPlayerObject = NULL;
         bqPlayerPlay = NULL;
-        bqPlayerQueue = NULL;
+        bqPlayerBufferQueue = NULL;
         bqPlayerVolume = NULL;
     }
 
@@ -162,7 +159,8 @@ Java_com_fmtech_ffmpegopensl_MusicPlayer_stop(JNIEnv *env, jobject instance) {
     if(NULL != engineObject){
         (*engineObject)->Destroy(engineObject);
         engineObject = NULL;
-        slEngineItf = NULL;
+        engineEngine = NULL;
     }
+
     releaseFFmpeg();
 }
